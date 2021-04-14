89c48241a68382c1a8606bf89e0d44489dc30fe0
firemaples <firemaples@gmail.com>
2021-04-14 11:35:34 +0800

    Upgradle Gradle, AGP and ButterKnife, and use AndroidX library

--- app/src/main/java/com/study/benchmarkorm/BasicFragment.java
+++ app/src/main/java/com/study/benchmarkorm/BasicFragment.java
@@ -4,9 +4,8 @@ import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
-import android.provider.Settings;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,59 +13,59 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import butterknife.BindView;
 import com.db.chart.Tools;
 import com.db.chart.animation.Animation;
 import com.db.chart.model.LineSet;
 import com.db.chart.renderer.AxisRenderer;
 import com.db.chart.view.LineChartView;
 
-import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class BasicFragment extends Fragment {
 
-    @Bind(R.id.play_full)
+    @BindView(R.id.play_full)
     ImageButton mPlayBtnFull;
-    @Bind(R.id.warning)
+    @BindView(R.id.warning)
     ImageButton mWarningBtn;
 
-    @Bind(R.id.play_read)
+    @BindView(R.id.play_read)
     ImageButton mPlayBtnRead;
-    @Bind(R.id.chart_read)
+    @BindView(R.id.chart_read)
     LineChartView mChartRead;
 
-    @Bind(R.id.play_write)
+    @BindView(R.id.play_write)
     ImageButton mPlayBtnWrite;
-    @Bind(R.id.chart_write)
+    @BindView(R.id.chart_write)
     LineChartView mChartWrite;
 
-    @Bind(R.id.play_update)
+    @BindView(R.id.play_update)
     ImageButton mPlayBtnUpdate;
-    @Bind(R.id.chart_update)
+    @BindView(R.id.chart_update)
     LineChartView mChartUpdate;
 
-    @Bind(R.id.play_delete)
+    @BindView(R.id.play_delete)
     ImageButton mPlayBtnDelete;
-    @Bind(R.id.chart_delete)
+    @BindView(R.id.chart_delete)
     LineChartView mChartDelete;
 
-    @Bind(R.id.tv_read)
+    @BindView(R.id.tv_read)
     TextView tv_read;
-    @Bind(R.id.tv_write)
+    @BindView(R.id.tv_write)
     TextView tv_write;
-    @Bind(R.id.tv_update)
+    @BindView(R.id.tv_update)
     TextView tv_update;
-    @Bind(R.id.tv_delete)
+    @BindView(R.id.tv_delete)
     TextView tv_delete;
 
-    @Bind(R.id.tv_read_result)
+    @BindView(R.id.tv_read_result)
     TextView tv_read_result;
-    @Bind(R.id.tv_write_result)
+    @BindView(R.id.tv_write_result)
     TextView tv_write_result;
-    @Bind(R.id.tv_update_result)
+    @BindView(R.id.tv_update_result)
     TextView tv_update_result;
-    @Bind(R.id.tv_delete_result)
+    @BindView(R.id.tv_delete_result)
     TextView tv_delete_result;
 
     private boolean isPlayedR = false;
--- app/src/main/java/com/study/benchmarkorm/MainActivity.java
+++ app/src/main/java/com/study/benchmarkorm/MainActivity.java
@@ -1,26 +1,26 @@
 package com.study.benchmarkorm;
 
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import com.google.android.material.tabs.TabLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
-import butterknife.Bind;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Bind(R.id.container)
+    @BindView(R.id.container)
     ViewPager mViewPager;
 
-    @Bind(R.id.tab_layout)
+    @BindView(R.id.tab_layout)
     TabLayout mTabLayout;
 
-    @Bind(R.id.toolbar)
+    @BindView(R.id.toolbar)
     Toolbar toolbar;
 
     SectionsPagerAdapter mSectionsPagerAdapter;
